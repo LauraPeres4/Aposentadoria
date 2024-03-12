70fe2982b61621699ce3c57926a3a58ab8804ea4
Usuario <Usuario@CPD02-M16>
2024-03-12 12:03:49 -0300

    Adicionar arquivos de projeto.

---
+++ Aposentadoria/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aposentadoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos anos tem o empregado: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos anos trabalhados tem o empregado: ");
+            int trabalho = int.Parse(Console.ReadLine());
+
+            if(idade >= 65 && trabalho >= 30 || idade >= 60 && trabalho >= 25)
+            {
+                Console.WriteLine("Tem direito a aponsentadoria.");
+            }
+            else
+            {
+                Console.WriteLine("Não tem direito a aposentadoria.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
